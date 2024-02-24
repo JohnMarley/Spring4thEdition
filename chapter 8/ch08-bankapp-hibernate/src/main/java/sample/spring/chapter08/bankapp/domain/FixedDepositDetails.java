04c4a16813581a9b9fa0b4e26cedb7e15f59ecb8
JohnMarley <kudalyyevgeniyy@gmail.com>
2024-02-24 22:00:53 +0200

    upd to work with hibernate

--- chapter 8/ch08-bankapp-hibernate/src/main/java/sample/spring/chapter08/bankapp/domain/FixedDepositDetails.java
+++ chapter 8/ch08-bankapp-hibernate/src/main/java/sample/spring/chapter08/bankapp/domain/FixedDepositDetails.java
@@ -1,5 +1,14 @@
 package sample.spring.chapter08.bankapp.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 import java.util.Date;
 
 import javax.persistence.Column;
