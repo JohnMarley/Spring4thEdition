b1c186e6d67d9a6caaa66bc33fbc692363451e08
JohnMarley <kudalyyevgeniyy@gmail.com>
2024-02-25 14:42:37 +0200

    upd to work with hibernate in javaconfig way

--- chapter 8/ch08-bankapp-hibernate/src/main/java/sample/spring/chapter08/bankapp/domain/FixedDepositDetails.java
+++ chapter 8/ch08-bankapp-hibernate/src/main/java/sample/spring/chapter08/bankapp/domain/FixedDepositDetails.java
@@ -11,14 +11,6 @@ import jakarta.persistence.Table;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity(name = "FixedDepositDetails")
 @Table(name = "fixed_deposit_details")
