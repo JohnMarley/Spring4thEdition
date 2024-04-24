d8fb88dd93a95e9468efee5d340b2dc6fa1c44a6
JohnMarley <kudalyyevgeniyy@gmail.com>
2024-04-24 22:31:29 +0300

    upd spring-mail project to make it work: emails are sent

--- chapter 10/ch10-bankapp-javaconfig/src/main/java/sample/spring/chapter10/bankapp/jms/MyAnnotatedJmsListener.java
+++ chapter 10/ch10-bankapp-javaconfig/src/main/java/sample/spring/chapter10/bankapp/jms/MyAnnotatedJmsListener.java
@@ -1,19 +1,29 @@
 package sample.spring.chapter10.bankapp.jms;
 
 
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import jakarta.jms.JMSException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import sample.spring.chapter10.bankapp.EmailConfig;
 import sample.spring.chapter10.bankapp.repository.BankAccountRepositoryCustom;
 import sample.spring.chapter10.bankapp.repository.FixedDepositRepository;
 import sample.spring.chapter10.bankapp.domain.FixedDepositDetails;
+
+import java.io.IOException;
 
 /**
  * This class shows usage of @JmsListener annotation.
@@ -22,12 +32,25 @@ import sample.spring.chapter10.bankapp.domain.FixedDepositDetails;
 public class MyAnnotatedJmsListener {
 	private static Logger logger = LogManager.getLogger(MyAnnotatedJmsListener.class);
 
-//	@Autowired
-//	private transient MailSender mailSender;
+	@Autowired
+	@Qualifier("mailSender")
+	private transient MailSender mailSender;
 
-//	@Autowired
-//	@Qualifier("requestReceivedTemplate")
-//	private transient SimpleMailMessage simpleMailMessage;
+	@Autowired
+	@Qualifier("requestReceivedTemplate")
+	private transient SimpleMailMessage simpleMailMessage;
+
+	@Autowired
+	@Qualifier("requestReceivedSGTemplate")
+	private Mail requestReceivedSGTemplate;
+
+	@Autowired
+	@Qualifier("sendGrid")
+	private SendGrid sendGrid;
+
+	@Autowired
+	@Qualifier("sendEmailRequestSG")
+	private Request requestSG;
 
 	@Autowired
 	private FixedDepositRepository fixedDepositRepository;
@@ -35,20 +58,44 @@ public class MyAnnotatedJmsListener {
 	@Autowired
 	private BankAccountRepositoryCustom bankAccountRepositoryCustom;
 
-//	public void sendEmail() {
-//		mailSender.send(simpleMailMessage);
-//	}
+
+	public void sendEmail() {
+		mailSender.send(simpleMailMessage);
+	}
+
+	/**
+	 * send email via sendgrid service
+	 * @param address - destination address
+	 */
+	public void sendEmailSG(String address) {
+		if (EmailConfig.isEmailsEnabled()) {
+			var personalization = new Personalization();
+			personalization.addTo(new Email(address));
+			requestReceivedSGTemplate.addPersonalization(personalization);
+			try {
+				requestSG.setBody(requestReceivedSGTemplate.build());
+				var response = sendGrid.api(requestSG);
+				if (response.getStatusCode()!= 202) {
+					logger.warn("Email was not sent. Response:\n" + response.getStatusCode() + response.getBody());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			logger.warn("Emails sending is disabled in app");
+		}
+	}
 
 	@JmsListener(destination = "EMAIL_QUEUE_DESTINATION")
 	public void processEmailMessage(Message<String> message) {
 		logger.info("processEmailMessage() invoked");
 		logger.info(message.toString());
 //		simpleMailMessage.setTo(message.getPayload());
-//		sendEmail();
+		sendEmailSG(message.getPayload());
 	}
 
 	@JmsListener(destination = "FIXED_DEPOSIT_DESTINATION")
-	public void processFixedDeposit(Message<FixedDepositDetails> message) throws JMSException {
+	public void processFixedDeposit(Message<FixedDepositDetails> message) {
 		logger.info("processFixedDeposit() invoked");
 		FixedDepositDetails fdd = message.getPayload();
 		if (fdd != null) {
--- chapter 10/ch10-bankapp-javaconfig/src/main/java/sample/spring/chapter10/bankapp/job/FixedDepositProcessorJob.java
+++ chapter 10/ch10-bankapp-javaconfig/src/main/java/sample/spring/chapter10/bankapp/job/FixedDepositProcessorJob.java
@@ -1,20 +1,33 @@
 package sample.spring.chapter10.bankapp.job;
 
+import java.io.IOException;
 import java.util.List;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
+//import javax.mail.MessagingException;
+//import javax.mail.internet.AddressException;
 
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import sample.spring.chapter10.bankapp.EmailConfig;
 import sample.spring.chapter10.bankapp.repository.FixedDepositRepository;
 import sample.spring.chapter10.bankapp.domain.FixedDepositDetails;
 
+@Slf4j
 @Component
 public class FixedDepositProcessorJob {
+
 	@Autowired
 	private FixedDepositRepository fixedDepositRepository;
 //
@@ -25,22 +38,52 @@ public class FixedDepositProcessorJob {
 //	@Qualifier("requestProcessedTemplate")
 //	private transient SimpleMailMessage simpleMailMessage;
 
+	@Autowired
+	@Qualifier("requestProcessedSGTemplate")
+	private Mail requestProcessedSGTemplate;
+
+	@Autowired
+	@Qualifier("sendGrid")
+	private SendGrid sendGrid;
+
+	@Autowired
+	@Qualifier("sendEmailRequestSG")
+	private Request requestSG;
+
+
 	private List<FixedDepositDetails> getInactiveFixedDeposits() {
 		return fixedDepositRepository.getInactiveFixedDeposits();
 	}
 
 	@Scheduled(fixedRate = 5000)
-	public void sendEmail() throws AddressException, MessagingException {
-//		List<FixedDepositDetails> inactiveFds = getInactiveFixedDeposits();
-//		for (FixedDepositDetails fd : inactiveFds) {
-//			MimeMessage mimeMessage = mailSender.createMimeMessage();
-//			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(
-//					mimeMessage);
-//			mimeMessageHelper.setTo(fd.getEmail());
-//			mimeMessageHelper.setSubject(simpleMailMessage.getSubject());
-//			mimeMessageHelper.setText(simpleMailMessage.getText());
-//			mailSender.send(mimeMessage);
-//		}
-//		myFixedDepositDao.setFixedDepositsAsActive(inactiveFds);
+	public void sendEmail() {
+		List<FixedDepositDetails> inactiveFds = getInactiveFixedDeposits();
+		for (FixedDepositDetails fd : inactiveFds) {
+			sendEmailSG(fd);
+		}
+		fixedDepositRepository.setFixedDepositsAsActive(inactiveFds);
+	}
+
+	/**
+	 * send email via sendgrid service
+	 * @param fd - FixedDepositDetails
+	 */
+	public void sendEmailSG(FixedDepositDetails fd) {
+		if (EmailConfig.isEmailsEnabled()) {
+			var personalization = new Personalization();
+			personalization.addTo(new Email(fd.getEmail()));
+			requestProcessedSGTemplate.addPersonalization(personalization);
+			try {
+				requestSG.setBody(requestProcessedSGTemplate.build());
+				var response = sendGrid.api(requestSG);
+				if (response.getStatusCode()!= 202) {
+					log.warn("Email was not sent. Response:\n" + response.getStatusCode() + response.getBody());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			log.warn("Emails sending is disabled in app");
+		}
 	}
 }
